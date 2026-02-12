d1e2b137932c26ee27f56b45c1f464e7112a84fd
Steve Trettel <stevejtrettel@gmail.com>
2026-02-12 05:18:48 -0800

    removed cirm course shaders

--- demos/course/day4_new/game-of-life/image.glsl
+++
@@ -1,4 +0,0 @@
-void mainImage(out vec4 fragColor, in vec2 fragCoord)
-{
-    fragColor = texelFetch(iChannel0, ivec2(fragCoord), 0);
-}
--- demos/course/day4_new/texture-flip/image.glsl
+++
@@ -1,6 +0,0 @@
-void mainImage(out vec4 fragColor, in vec2 fragCoord)
-{
-    vec2 uv = fragCoord / iResolution.xy;
-    uv.x = 1.0 - uv.x;
-    fragColor = texture(iChannel0, uv);
-}
